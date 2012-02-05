4372d308fb29cfb67bb87cff9442ac397178a0cf
Mitchel Humpherys <mitch.special@gmail.com>
2012-02-05 09:08:22 -0800

    only deleting 1000 at a time

--- yalt/tasks.py
+++ yalt/tasks.py
@@ -3,6 +3,7 @@ import datetime
 
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
+from google.appengine.ext import db
 
 from models import Location
 
@@ -13,14 +14,16 @@ is_dev_server = os.environ['SERVER_SOFTWARE'].startswith('Dev')
 class CleanUp(webapp.RequestHandler):
     def get(self):
         print 'hello, world'
+        limit = 1000
+        if 'limit' in self.request.arguments():
+            limit = int(self.request.get('limit'))
+
         one_week_ago = datetime.datetime.now() - datetime.timedelta(7,0,0)
-        lq = Location.all().filter('date < ', one_week_ago)
-        cnt = 0
-        for l in lq:
-            cnt += 1
-            l.delete()
-        print 'finished cleaning up stuff older than a week (deleted ' \
-            + str(cnt) + ' items)'
+        lq = db.GqlQuery('SELECT __key__ FROM Location WHERE date < :1', one_week_ago)
+
+        db.delete(lkey for lkey in lq.fetch(limit))
+
+        print 'finished cleaning up stuff older than a week'
 
 url_spec = [('/tasks/cleanup', CleanUp)]
 
