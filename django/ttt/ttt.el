dea0ffa16575ab76ea0b952affe24a4b37416907
Mitchel Humpherys <mitch.special@gmail.com>
2013-09-25 00:58:15 -0700

    ttt.el: goto settings

--- django/ttt/ttt.el
+++ django/ttt/ttt.el
@@ -5,3 +5,16 @@
 (defun ttt-runserver ()
   (interactive)
   (ttt-manage-command "runserver"))
+
+(defun ttt-goto-setting (setting)
+  (let ((settings-buffer (get-buffer "settings.py")))
+    (switch-to-buffer settings-buffer)
+    (goto-char 0)
+    (search-forward-regexp (format "\\b%s\\b"
+                                   setting))
+    (move-beginning-of-line nil)
+    (recenter)))
+
+(defun ttt-goto-installed-apps ()
+  (interactive)
+  (ttt-goto-setting "INSTALLED_APPS"))
