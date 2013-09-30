6c1fae84847f26b31945aab3076f9ed9a84515de
Mitchel Humpherys <mitch.special@gmail.com>
2013-09-30 07:07:30 -0700

    ttt: the rest of the views

--- django/ttt/tracking/views.py
+++ django/ttt/tracking/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.conf import settings
 from django.utils.simplejson import dumps
+from django.contrib.admin.views.decorators import staff_member_required
 
 import datetime
 
@@ -38,18 +39,18 @@ def index(request):
     return render(request, 'index.html')
 
 
-def json_response(msg, success, data=None):
+def json_response(msg, success, **kwargs):
     obj = {'msg': msg, 'success', success}
-    if data is not None:
-        obj['data'] = data
+    for k,v in **kwargs.iteritems():
+        obj[k] = v
     json = dumps(obj)
     return HttpResponse(json, mimetype='application/json')
 
-def json_failure(msg, data=None):
-    return json_response(msg, False)
+def json_failure(msg, **kwargs):
+    return json_response(msg, False, **kwargs)
 
-def json_success(msg, data=None):
-    return json_response(msg, False)
+def json_success(msg, **kwargs):
+    return json_response(msg, True, **kwargs)
 
 def ttt_put(request):
     required_args = ['lat', 'lon', 'acc', 'secret', 'speed']
@@ -113,7 +114,8 @@ def ttt_get(request):
     return json_success('Succcess!', data={'locations': location_dicts})
 
 def live(request):
-    return render(request, 'live.html')
+    sec = request.GET.get('secret')
+    return render(request, 'live.html', {'lsecret':sec})
 
 def help(request):
     return render(request, 'help.html')
@@ -127,12 +129,76 @@ def download(request):
 def secret(request):
     return render(request, 'secret.html')
 
+@staff_member_required
 def new_test_point(request):
-    return render(request, 'new_test_point.html')
+    # check for required args:
+    required_args = ['lat_step', 'lon_step', 'acc_step', 'speed_step']
+    valid_args = True
+    for a in required_args:
+        if a not in request.GET:
+            valid_args = False
+            break
+    if not valid_args:
+        msg = 'You must supply ' + ', '.join(required_args) + ' arguments.'
+        return json_failure(msg)
 
+    user = request.user
+    newl = Location()
+
+    try:
+        l = Location.objects().filter(user__exact=user).latest('date')
+        newl.latitude = l.latitude + float(request.GET['lat_step'])
+        newl.longitude= l.longitude + float(request.GET['lon_step'])
+        newl.accuracy = l.accuracy + float(request.GET['acc_step'])
+        newl.speed = l.speed + float(request.GET['speed_step'])
+    except Location.DoesNotExist:
+        newl.latitude = 32.95736
+        newl.longitude = -117.233133
+        newl.accuracy = 10.0
+        newl.speed = 42.0
+
+    newl.user = user
+    newl.save()
+
+    msg = 'Test point added: ' + str(newl.to_dict())
+    return json_success(msg, title='Success!')
+
+@staff_member_required
 def admin(request):
-    return render(request, 'admin.html')
+    user = request.user
+    data = []
+    secrets = Secret.all()
+    for s in secrets:
+        data.append({
+            'secret': s,
+            'num_locations': Location.objects.filter(user__exact=s.user).count()
+        })
 
+    return render(request, 'admin.html', {
+        'secrets': secrets,
+        'data': data,
+        'user': user
+    })
+
+@staff_member_required
 def view_data(request):
-    return render(request, 'view_data.html')
+    user = request.user
 
+    if 'secret' not in request.GET:
+        return render(request, 'view_data.html', {
+            'success': False
+        })
+
+    secret_txt = request.GET['secret']
+    try:
+        secret = Secret.objects().filter(secret__exact=secret_txt).get()
+    except Secret.DoesNotExist:
+        return render(request, 'view_data.html', {
+            'success': False
+        })
+
+    return render(request, 'view_data.html', {
+            'data': Location.objects().filter(user__exact=secret.user).order('-date')[:500],
+            'secret_for_page': secret_txt,
+            'success': True
+            })
