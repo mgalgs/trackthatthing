80f9384ac2b94b1d6336cc82c067af12444e4dc4
Mitchel Humpherys <mitch.special@gmail.com>
2014-10-28 23:30:21 -0700

    ttt.el: colorize stuff

--- django/ttt/ttt.el
+++ django/ttt/ttt.el
@@ -20,3 +20,12 @@
 (defun ttt-goto-installed-apps ()
   (interactive)
   (ttt-goto-setting "INSTALLED_APPS"))
+
+(require 'ansi-color)
+
+(defun colorize-compilation-buffer ()
+  (toggle-read-only)
+  (ansi-color-apply-on-region (point-min) (point-max))
+  (toggle-read-only))
+
+(add-hook 'compilation-filter-hook 'colorize-compilation-buffer)
