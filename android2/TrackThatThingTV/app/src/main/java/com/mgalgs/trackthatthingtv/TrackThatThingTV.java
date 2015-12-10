9c7ce8ec205099546691b04121715ef6e20dbb0f
Mitchel Humpherys <mitch.special@gmail.com>
2015-12-09 23:17:22 -0800

    A few more https's

--- android2/TrackThatThingTV/app/src/main/java/com/mgalgs/trackthatthingtv/TrackThatThingTV.java
+++ android2/TrackThatThingTV/app/src/main/java/com/mgalgs/trackthatthingtv/TrackThatThingTV.java
@@ -5,5 +5,5 @@ package com.mgalgs.trackthatthingtv;
  */
 public class TrackThatThingTV {
     public static final String TAG = "TrackThatThingTV";
-    public static final String BASE_URL = "http://trackthatthing.com";
+    public static final String BASE_URL = "https://trackthatthing.com";
 }
